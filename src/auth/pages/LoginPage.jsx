c8c3f17bac406b42bf3684c89d897841d01108c7
Joel Hernandez <joelhernandez@Joels-MacBook-Pro.local>
2022-09-16 08:15:37 -0400

    adding style to loginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,22 +1,34 @@
-import { AuthLayout } from "../layout/AuthLayout"
+import { AuthLayout } from "../layout/AuthLayout";
+import { Link } from "react-router-dom";
+import { FaGoogle} from "react-icons/fa";
 
 export const LoginPage = () => {
   return (
    <AuthLayout>
     <div className="auth-container">
-     <h1>Login</h1>
+     <h1 className="text-center">Log in</h1>
 
-      <form>
-       <label className="d-block" for="email" id="emailInput" aria-label="email">
-         <input type="email" name="email" placeholder="demo@gmail.com"/>
+      <form className="auth-logIn-form">
+       <label className="d-block" htmlFor="email" aria-label="email">
+         <input type="email" name="email" id="emailInput"  placeholder="demo@gmail.com"/>
        </label>
        
 
-       <label for="password">Password
-          <input type="password" name="password"/>
+       <label className="d-block" htmlFor="password"  aria-label="password">
+          <input type="password" name="password" id="password-input" />
        </label>
        
+       <button className="btn-blue" type="submit">Log in</button>
      </form>
+
+     <span className="d-block text-center uppercase fs-smallest mt-20">or</span>
+
+     <button className="btn-black btn-google mt-25">
+      <FaGoogle/>
+       Continue with Google
+       </button>
+
+      <Link className="d-block mt-20 text-center" to={"/register"}> Can’t log in? Create an account?</Link>
 
     </div>
    </AuthLayout>
