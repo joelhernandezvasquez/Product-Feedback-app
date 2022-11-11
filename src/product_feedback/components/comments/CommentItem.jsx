83b62f8774f1215e73a10743dcfec50529976ba5
Joel Hernandez <joelhernandez@Joels-MacBook-Pro.local>
2022-11-11 17:50:40 -0500

    add styiling to render comment

--- src/product_feedback/components/comments/CommentItem.jsx
+++ src/product_feedback/components/comments/CommentItem.jsx
@@ -1,7 +1,26 @@
-
-
-export const CommentItem = ({comment}) => {
+import Proptypes from 'prop-types';
+export const CommentItem = ({photoURL,userName,userEmail,comment}) => {
   return (
-    <li>{comment}</li>
+    <li className='comment-item'>
+     
+        <img className='comment-item-avatar' src={photoURL} alt={`${userName} profile`}/>
+        <p className='comment-item-profile-info'>
+          <span className='comment-profile-name'>{userName}</span>
+          <span className='comment-profile-email'>{userEmail}</span>
+        </p>
+     
+        <button className='btn-reply capitalize'>reply</button>
+      
+      <p className='comment-text'>
+        {comment}
+      </p>
+    </li>
   )
+}
+
+CommentItem.prototype = {
+  photoURL:Proptypes.string.isRequired,
+  userName:Proptypes.string.isRequired,
+  userEmail:Proptypes.string.isRequired,
+  comment:Proptypes.string.isRequired
 }
--- src/product_feedback/components/comments/CommentList.jsx
+++ src/product_feedback/components/comments/CommentList.jsx
@@ -1,13 +1,20 @@
-import { CommentItem } from "./CommentItem"
-
+import Proptypes from 'prop-types';
+import { CommentItem } from "./CommentItem";
 
 export const CommentList = ({comments}) => {
   
   return (
-    <ul>
+    <section className='comment-wrapper primary-border-radius'>
+    <h2>4 Comments</h2>
+    <ul className='comment-list primary-border-radius'>
       {comments.map((comment)=>{
         return <CommentItem key={comment.id} {...comment}/>
       })}
       </ul>
+
+      </section>
   )
+}
+CommentList.proptypes = {
+  comments:Proptypes.array.isRequired
 }
